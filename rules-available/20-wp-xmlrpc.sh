a416385dddc0408e31add1efc1011570297cbcfb
mrk25 <subaddiction@subaddiction.net>
2016-07-06 10:40:02 +0200

    Update 20-wp-xmlrpc.sh

--- rules-available/20-wp-xmlrpc.sh
+++ rules-available/20-wp-xmlrpc.sh
@@ -7,6 +7,12 @@ MAXATTEMPTS=2
 
 cat $LOGFILE | grep "POST /xmlrpc.php" | awk '{print $2}'| sort | uniq -dc > $FILE
 
+# In plesk we must iterate over all access_log files. matching "access_*log" includes ssl logs
+#find /var/www/vhosts -name "access_*log" -exec grep -i "POST /\{1,3\}xmlrpc.php" {} \; > wp-xmlrpc.tmp
+#cat wp-xmlrpc.tmp | awk '{print $1}' | sort | uniq -dc > $FILE
+#rm wp-xmlrpc.tmp
+
+
 ATTACKER=false
 ATTACK=false
 
